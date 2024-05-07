9028d9e62f351308bdd6186ffedb9ef8fe82dbf4
Josh <jovazquez1@s.sfusd.edu>
2024-05-07 10:13:25 -0700

    Added Player, World, and moving platform
    Added features up to minute 28:20 in video

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+# MINUTE 25:44
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
