06a3b1690fae921588801a38ff48f2caabb9b4d6
Josh <jovazquez1@s.sfusd.edu>
2024-05-29 09:32:52 -0700

    added coyote jump

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -4,13 +4,16 @@ var direction = 1
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var slime_bounce = $SlimeBounce
 
 
 func _process(delta):
 	if ray_cast_right.is_colliding():
+		slime_bounce.play()
 		direction = -1
 		animated_sprite.flip_h = true
 	if ray_cast_left.is_colliding():
+		slime_bounce.play()
 		direction = 1 
 		animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
