1b0bd447ef98a7f174dd592b219092eaf96ffc22
Josh <jovazquez1@s.sfusd.edu>
2024-05-29 08:42:56 -0700

    Added death SFX and jump SFX

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,12 +1,16 @@
 extends Area2D
 @onready var timer = $Timer
+@onready var death_sound = $DeathSound
+
 
 func _on_body_entered(body):
 	print("You died!")
-	Engine.time_scale= 0.5
+	death_sound.play()
+	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
+	death_sound.play()
 	timer.start()
 
 func _on_timer_timeout():
-	Engine.time_scale= 1.0
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,12 +3,15 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var jump_sound = $JumpSound
+
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction > 0: 
 		animated_sprite.flip_h = false
